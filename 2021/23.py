ae288a85a4239b2472549539b8542f54cdf691c8
Harvey Jones <harveyj@gmail.com>
2022-01-02 20:55:12 -0500

    days 23, 24 and notes

---
+++ 2021/23.py
@@ -0,0 +1,92 @@
+import itertools
+import networkx
+import copy
+import collections
+import puzzle
+
+def run(INPUT):
+  lines = INPUT.split('\n')
+
+  state = {}
+  for y, l in enumerate(lines):
+    for x, c in enumerate(l):
+      if c not in '. #':
+        state[(c, str(x) + str(y))] = (x, y)
+  start_state = repr(state)
+
+  COLS = {'A': 3, 'B': 5, 'C': 7, 'D': 9}
+  COSTS = {'a': 1, 'A': 1, 'b': 10, 'B': 10, 'c': 100, 'C': 100, 'd': 1000, 'D': 1000}
+  LEGAL = [1, 2, 4, 6, 8, 10, 11]
+
+  def is_end(state):
+    # print([c for ((c, uid), (x,y)) in state.items() if x == COLS[c]])
+    return len([c for ((c, uid), (x,y)) in state.items() if x == COLS[c]]) == len(state)
+
+  def all_edges(state):
+    moves = []
+    if state == 'END': return []
+    if is_end(state):
+      # print('found end', state)
+      return [(state, 'END', 0)]
+    tops = [((c, uid), x) for ((c, uid), (x, y)) in state.items() if y == 1]
+    top_occupied = [x for (_, (x, y)) in state.items() if y == 1]
+    cols = {x: [(loc[1], key) for (key, loc) in state.items() if loc[0] == x] for x in COLS.values()}
+    for (c, uid), x in tops:
+      if [x2 for x2 in top_occupied if x <  x2 < COLS[c] or x > x2 > COLS[c]]: continue
+      tgt_col = [(y, c2) for (y, c2) in cols.get(COLS[c], [])]
+      blockers = [c2 for (y, (c2, uid)) in tgt_col if c2 != c]
+      if not blockers:
+        top_y, _ = min(tgt_col) if tgt_col else ((2 + len(state)//4), None)
+        new_state = copy.copy(state)
+        new_state[(c, uid)] = (COLS[c], top_y -1)
+        cost = COSTS[c] * (abs(x - COLS[c])+top_y -1)
+        # print('move', c , 'to', COLS[c], "cost", cost)
+        moves.append((state, new_state, cost))
+
+    for x in cols:
+      # skip this if empty
+      if not cols[x]: continue
+      correct = [c for y, (c, uid) in cols[x] if x == COLS[c]]
+      # skip this if there are only correct values 
+      if len(correct) == len(cols[x]): continue
+      top_y, (top_c, top_uid) = min(cols[x])
+
+      top_x = [0] + sorted([x for (c, x) in tops]) + [12]
+      for x1, x2 in zip(top_x, top_x[1:]):
+        if x1 < x < x2: 
+          for n_x in [l for l in LEGAL if x1 < l < x2]:
+            new_state = copy.copy(state)
+            new_state[(top_c, top_uid)] = (n_x, 1)
+            cost = COSTS[top_c] * (abs(n_x - x) + (top_y - 1))
+            # print('move', top_c , 'to', n_x, "cost", cost)
+            moves.append((state, new_state, cost))
+    return moves
+
+  DG = networkx.DiGraph()
+  q = collections.deque([state])
+  seen = set()
+  while q:
+    state = q.pop()
+    if repr(state) in seen: continue
+    seen.add(repr(state))
+    ae = all_edges(state)
+    graph_ae = [(repr(orig_state), repr(new_state), cost) for orig_state, new_state, cost in ae]
+    # print("graph_ae", graph_ae)
+    # print('ae', ae)
+    DG.add_weighted_edges_from(graph_ae)
+    q.extend([new_state for _, new_state, _2 in ae])
+  sp = networkx.shortest_path(DG, start_state, "'END'", weight="weight")
+  cost = 0
+  for s1, s2 in zip(sp, sp[1:]):
+    s1, s2 = eval(s1), eval(s2)
+    diff = [(k, s1[k], s2[k]) for k in s1 if s2 != "END" and s1[k] != s2[k]]
+    print(diff)
+    if diff:
+      diff = diff[0]
+      cost += COSTS[diff[0][0]] * (abs(diff[1][0] - diff[2][0])+abs(diff[1][1] - diff[2][1]))
+  print(cost)
+
+p = puzzle.Puzzle("23")
+# p.run(run, 0)
+p.run(run, 3)
+
---
+++ 2021/24.py
@@ -0,0 +1,153 @@
+import itertools
+import networkx
+import copy
+import collections
+import puzzle
+
+# https://appdividend.com/2021/03/23/how-to-check-if-string-is-integer-in-python/
+def checkInt(str):
+    if str[0] in ('-', '+'):
+        return str[1:].isdigit()
+    return str.isdigit()
+
+class ALU(object):
+  def __init__(self, inputs=[]):
+    self.inputs = inputs
+    self.regs = {'w':0, 'x':0, 'y':0, 'z':0}
+
+  def run(self, prog):
+    for line in prog.split("\n"):
+      self.exec(line)
+      # print(self.regs)
+
+  def exec(self, inst):
+    op, a = inst.split(" ")[:2]
+    if op == "inp":
+      self.regs[a] = self.inputs.pop(0)
+      return
+    b = inst.split(" ")[2]
+    if checkInt(b):
+      b = int(b)
+    else: b = self.regs[b]
+    if op == "add":
+      self.regs[a] += b
+    elif op == "mul":
+      self.regs[a] *= b
+    elif op == "div":
+      self.regs[a] //= b
+    elif op == "mod":
+      self.regs[a] %= b
+    elif op == "eql":
+      self.regs[a] = 1 if self.regs[a] == b else 0
+
+def simple_run(INPUT, user_input):
+  alu = ALU(user_input)
+  alu.run(INPUT)
+  return alu
+
+def simple_run_last_segment(INPUT):
+  for digit in range(1, 10):
+    for z in range(0, 200):
+      alu = ALU([digit])
+      alu.regs['z'] = z
+      alu.run(INPUT)
+      if alu.regs['z'] == 0:
+        print(digit, z, alu.regs['z'])
+
+def iter_z(in_val, z, add_x, add_y, div_z):
+  w = in_val
+  x = z % 26
+  z //= div_z
+  x += add_x
+  x = 0 if x == w else 1
+  y = 25
+  y *= x
+  y += 1
+  z *= y
+  y = w + add_y
+  y *= x
+  z += y
+  return z
+
+def iter_z_2(w, z, add_x, add_y, div_z):
+  # print('if', 0 if z % 26 + add_x == w else 1)
+  return z // div_z * (25 * (0 if z % 26 + add_x == w else 1) + 1) + (w + add_y) * (0 if z % 26 + add_x == w else 1)
+
+def iter_z_3(w, z, add_x, add_y, div_z):
+  if (z % 26 + add_x) == w:
+    return z // div_z 
+  else:
+    return z // div_z * 26 + (w+add_y)
+
+def python_version(inputs, z_start=0, slice=14):
+  x_s = [12, 11, 13, 11, 14, -10, 11, -9, -3, 13, -5, -10, -4, -5][:slice]
+  y_s = [4, 11, 5, 11, 14, 7, 11, 4, 6, 5, 9, 12, 14, 14][:slice]
+  z_s = [1, 1, 1, 1, 1, 26, 1, 26, 26, 1, 26, 26, 26, 26][:slice]
+  z = z_start
+  for w_in, x_in, y_in, z_in in zip(inputs, x_s, y_s, z_s):
+    z = iter_z_3(w_in, z, x_in, y_in, z_in)
+  return z
+
+def print_lasts():
+  lasts = ([(in_val, z, iter_z(in_val, z)) for in_val in range(1, 10) for z in range(0, 27)])
+  for last in lasts:
+    if last[1] == 0:
+      print(last)
+
+def assert_eq(user_input):
+  alu = puzzle.Puzzle("24").run(simple_run, 0, user_input)
+  if alu.regs["z"] != python_version(user_input):
+    print("FAIL", alu.regs["z"],python_version(user_input))
+  if alu.regs["z"] == 0:
+    print("ZERO", user_input)
+
+
+def base_26(val):
+  return [val // (26 ** n) % 26 for n in range(7, -1, -1)]
+
+# 0 is main
+# 4 is the last cycle of main
+# 5 is just the last cycle
+# p = puzzle.Puzzle("24")
+# p.run(simple_run_last_segment, 4)
+
+# val = [9, 9, 9, 9, 9, 5, 7,   9, 9, 9, 9, 9, 9, 9]
+# # assert_eq(val)
+# print(val, base_26(python_version(val, slice=10)))
+
+# for a in range(1, 10):
+#  for b in range(1, 2):
+#    for c in range(1, 2): 
+#      for d in range(1, 2): 
+#        for e in range(1, 2): 
+#          for f in range(1, 2): 
+#            val = [9, 7, 9, 9, 9, 9, 9, 9, 9, 9, 9, a, 4, 5]
+#            assert_eq(val)
+#            print(val, base_26(python_version(val)))
+
+
+
+x_s = [0, 12, 11, 13, 11, 14, -10, 11, -9, -3, 13, -5, -10, -4, -5]
+y_s = [0, 4,  11,  5, 11, 14,   7, 11,  4,  6,  5,  9,  12, 14, 14]
+z_s = [0, 1,  1,   1,  1,  1,  26,  1, 26, 26,  1, 26,  26, 26, 26]
+
+z_stack = [(0)]
+constraints = []
+for w, x_in, y_in, z_in in zip(range(0, 15), x_s, y_s, z_s):
+  z_prev = z_stack[-1]
+  if x_in > 10:
+    z_stack.append(('w'+str(w), y_in))
+  else:
+    if z_in == 26:
+      z_stack.pop()
+    constraints.append((z_prev, x_in, 'w'+str(w)))
+  print(w, z_stack)
+for (z_prev, x_in, w_in) in constraints:
+  print (z_prev, "%26", x_in, " = ", w_in)
+
+for i in range(1, 15):
+  print(base_26(python_version([9, 2, 9, 1, 5, 9, 7, 9, 9, 9, 9, 4, 9, 8], slice=i)))
+
+
+  21611513911181
+assert_eq([int(c) for c in list("21611513911181")])
