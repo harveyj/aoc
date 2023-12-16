8fe8546879b7947ffe7eac6707ef1c8b8c6c8325
Harvey Jones <harveyj@gmail.com>
2023-12-16 10:16:48 -0500

    once you pop the fun don't stop

---
+++ 2023/9.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import puzzle, re
+
+def parse_input(INPUT):
+  return [list(map(int, l.split())) for l in INPUT.split('\n')]
+
+def onetwo(INPUT):
+  invals = parse_input(INPUT)
+  trees = []
+  for line in invals:
+    allvals = []
+    curvals = line
+    while True:
+      allvals.append(curvals)
+      curvals = [curvals[i+1] - curvals[i] for i in range(len(curvals)-1)]
+      if curvals.count(0) == len(curvals):
+        break
+    trees.append(allvals)
+
+  out_1 = 0
+  for t in trees:
+    subtotal = 0
+    for i in range(len(t)-1, -1, -1):
+      subtotal += t[i][-1]
+    out_1 += subtotal
+  out_2 = 0
+  for t in trees:
+    subtotal = 0
+    for i in range(len(t)-1, -1, -1):
+      subtotal = t[i][0] - subtotal
+    out_2 += subtotal
+
+  return out_1, out_2
+
+p = puzzle.Puzzle("9")
+p.run(onetwo, 0)
