65228980664d3bf0041c148b143e17f8ed5946d8
Harvey Jones <harveyj@gmail.com>
2023-12-19 15:43:18 -0500

    day 16, day 17!

---
+++ 2023/16.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import puzzle, re
+from collections import deque
+
+def parse_input(INPUT):
+  return puzzle.Grid(raw=INPUT)
+
+def fslash(dir):
+  return -dir[1], -dir[0]
+
+def bslash(dir):
+  return dir[1], dir[0]
+
+def solve(start, dir, G):
+  # loc, (dx, dy)
+  queue = deque([(start, dir)])
+  locs = set()
+  seen = set()
+  while queue:
+    loc, dir = queue.pop()
+    locs.add(loc)
+    if (loc, dir) in seen: continue
+    seen.add((loc, dir))
+    # G.overlays[loc] = 'X'
+    new_loc = puzzle.pt_add(loc, dir)
+    cell = G.get(new_loc)
+    if cell == '.':
+      queue.append((new_loc, dir))
+    elif cell == '|':
+      if dir[0]:
+        queue.append((new_loc, (0, -1)))
+        queue.append((new_loc, (0, 1)))
+      else: 
+        queue.append((new_loc, dir))
+    elif cell == '-':
+      if dir[1]:
+        queue.append((new_loc, (-1, 0)))
+        queue.append((new_loc, (1, 0)))
+      else: 
+        queue.append((new_loc, dir))
+    elif cell == '/':
+      queue.append((new_loc, fslash(dir)))
+    elif cell == '\\':
+      queue.append((new_loc, bslash(dir)))
+    # out of bounds returns '#' which we treat like a wall
+  # print(G)
+  # first location is technically invalid
+  return len(locs) - 1 
+
+def one(INPUT):
+  G = parse_input(INPUT)
+  return solve((-1, 0), (1, 0), G)
+
+def two(INPUT):
+  G = parse_input(INPUT)
+  tiles = dict()
+  for x in range(G.max_x()):
+    tiles[(x, 0)] = solve((x, -1), (0, 1), G)
+    tiles[(x, G.max_y())] = solve((x, G.max_y() + 1), (0, -1), G)
+  for y in range(G.max_y()):
+    tiles[(0, y)] = solve((-1, y), (1, 0), G)
+    tiles[(G.max_x(), y)] = solve((y, G.max_x() + 1), (-1, 0), G)
+  for k in tiles:
+    print(k, tiles[k])
+  return max(tiles.values())
+
+p = puzzle.Puzzle("16")
+# p.run(one, 0)
+p.run(two, 0)
---
+++ 2023/17.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import puzzle, networkx
+from itertools import product
+
+def parse_input(INPUT):
+  return puzzle.Grid(raw=INPUT)
+
+deltas = {'N': (0, -1), 
+          'S': (0, 1), 
+          'E': (1, 0), 
+          'W': (-1, 0), 
+          }
+
+legal_turns = {'N': ['E', 'W'],
+                'W': ['N', 'S'],
+                'E': ['N', 'S'],
+                'S': ['E', 'W'],
+                '*': ['N', 'S', 'E', 'W']
+                }
+
+def mul_vect(vect, mag):
+  return [i * mag for i in vect]
+
+def find_longest_path(INPUT, min, max):
+  def add_edges(G, pt, prev_dir):
+    x, y = pt
+    for new_dir in legal_turns[prev_dir]:
+      for steps in range(min, max+1):
+        dx, dy = deltas[new_dir]
+        tgt = puzzle.pt_add((x, y), mul_vect((dx, dy), steps))
+        weight = sum([int(grid.get((x + dx * i, y + dy * i), default=10000)) for i in range(1,steps+1)])
+        if grid.get(tgt, default=None):
+
+          G.add_edge(((x, y), prev_dir), (tgt, new_dir), weight=weight)
+
+  grid = parse_input(INPUT)
+  G = networkx.DiGraph()
+  for x in range(grid.max_x()):
+    for y in range(grid.max_y()):
+      for prev_dir in ['N', 'S', 'E', 'W']:
+        G.add_node(((x, y), prev_dir))
+        add_edges(G, (x, y), prev_dir)
+  G.add_node('START')
+  add_edges(G, (0,0), '*')
+  G.add_edge('START', ((0,0), '*'), weight=0)
+  G.add_node('END')
+  for prev_dir in ['N', 'S', 'E', 'W']:
+    G.add_edge(((grid.max_x() -1, grid.max_y() -1), prev_dir), 'END', weight=0)
+
+  return networkx.dijkstra_path_length(G, source='START', target='END')
+
+def one(INPUT):
+  return find_longest_path(INPUT, 1, 3)
+
+def two(INPUT):
+  return find_longest_path(INPUT, 4, 10)
+
+
+p = puzzle.Puzzle("17")
+p.run(one, 0)
+p.run(two, 0)
