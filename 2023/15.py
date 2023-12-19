0b0fcdcfe88b2d7d5183ab3024b3bea710c99bb8
Harvey Jones <harveyj@gmail.com>
2023-12-18 20:20:42 -0500

    day 15

---
+++ 2023/15.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import puzzle, re
+
+def parse_input(INPUT):
+  return INPUT.split(',')
+
+def hash(instr):
+  cv = 0
+  for c in instr:
+    cv = ((cv + ord(c)) * 17) % 256
+  return cv
+
+def one(INPUT):
+  invals = parse_input(INPUT)
+  outs = [hash(iv) for iv in invals]
+  return sum(outs)
+
+def sum_box(box):
+  return sum([(i+1) * entry[1] for i, entry in enumerate(box)])
+
+def two(INPUT):
+  invals = parse_input(INPUT)
+  boxes = [[] for i in range(256)]
+  for iv in invals:
+    if '-' in iv:
+      label = iv[:-1]
+      box = hash(label)
+      for i, lens in enumerate(boxes[box]):
+        if label == lens[0]:
+          del boxes[box][i]
+    else:
+      if iv[-2] != '=':
+        print(iv)
+      label, power = iv.split('=')
+      box = hash(label)
+      if power != str(int(power)):
+        print('ERROR', power)
+      power = int(power)
+      replace = False
+      for i, lens in enumerate(boxes[box]):
+        if label == lens[0]:
+          lens[1] = power
+          replace = True
+      if not replace:
+        boxes[box].append([label, power])
+    print(iv, [(i, boxes[i]) for i in range(255) if len(boxes[i]) > 0])
+  # print(iv, [(i, boxes[i]) for i in range(255) if len(boxes[i]) > 0])
+  fps = [(i+1) * sum_box(boxes[i]) for i in range(256) if len(boxes[i]) > 0]
+  # print(boxes)
+  # print(fps)
+  print(hash('dk'))
+  return sum(fps)
+
+#dk=2 doesn't appear to do anything
+
+p = puzzle.Puzzle("15")
+# p.run(one, 0)
+p.run(two, 0)
