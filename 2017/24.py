a025152d9075569ed31551a3eca5f4259fa2e2d4
Harvey Jones <harveyj@gmail.com>
2024-11-09 11:20:34 -0500

    2017 done!!

---
+++ 2017/24.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import puzzle, library
+import re
+import networkx as nx
+from collections import deque
+def parse(INPUT):
+  for l in INPUT:
+    yield tuple(map(int, l.split('/')))
+
+
+def onetwo(INPUT):
+  G = nx.Graph()
+  for s, e in parse(INPUT):
+    if G.has_edge(s, e):
+      G.add_edge(s, e, G.get_edge_data(s, e) + 1)
+    else:
+      G.add_edge(s, e, data=1)
+
+  all_paths = []
+  paths = deque([[0]])
+  while True:
+    if len(paths) == 0: break
+    path = paths.pop()
+    all_paths.append(path)
+    for s, e in G.edges(path[-1]):
+      num_permitted = G.get_edge_data(s, e)
+      num_present = list(zip(path, path[1:])).count((s, e))
+      num_present += list(zip(path[1:], path)).count((s, e))
+      # print(f'npres: {num_present}, nperm:{num_permitted}')
+      if num_present >= num_permitted['data']: continue
+      paths.appendleft(path + [e])
+  print(all_paths)
+  path_len_weights = [(len(path), sum([a+b for a, b in zip(path, path[1:])])) for path in all_paths]
+  max_weight = max([b for _, b in path_len_weights])
+  max_len = max([a for a, _ in path_len_weights])
+  two = max([b for a, b in path_len_weights if a == max_len])
+  return max_weight, two
+
+p = puzzle.Puzzle("2017", "24")
+p.run(onetwo, 0)
---
+++ 2017/25.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import puzzle, library
+import re
+from collections import defaultdict
+
+
+def parse_chunk(chunk):
+  lines = chunk.split('\n')
+  # print(lines)
+  state = lines[0].split()[-1][0]
+  zero_out = int(lines[2].split()[-1][0])
+  zero_slot = lines[3].split()[-1][:-1]
+  zero_state = lines[4].split()[-1][0]
+  one_out = int(lines[6].split()[-1][0])
+  one_slot = lines[7].split()[-1][:-1]
+  one_state = lines[8].split()[-1][0]
+  return state, (zero_out, zero_slot, zero_state), (one_out, one_slot, one_state)
+
+
+def one(INPUT):
+  state = INPUT[0].split()[-1][0]
+  steps = int(INPUT[1].split()[-2])
+  tape = defaultdict(int)
+  tape_idx = 0
+
+  raw = '\n'.join(INPUT[2:]).strip()
+  chunks = map(parse_chunk, raw.split('\n\n'))
+  chunks_dict = {state: (zero, one) for state, zero, one in chunks}
+  for i in range(0, steps):
+    zero, one = chunks_dict[state]
+    out, slot, state = (zero if tape[tape_idx] == 0 else one)
+    tape[tape_idx] = out
+    tape_idx += (1 if slot == 'right' else -1)
+    # print(out, slot, state)
+  return list(tape.values()).count(1)
+
+def two(INPUT):
+  return 0
+
+p = puzzle.Puzzle("2017", "25")
+p.run(one, 0)
+p.run(two, 0)
