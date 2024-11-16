2ffd3b3ed1f4f8c84002fb3e7dc2ec34fdddc357
Harvey Jones <harveyj@gmail.com>
2024-11-16 12:31:12 -0500

    modernize 12

--- 2015/12.py
+++ 2015/12.py
@@ -3,22 +3,27 @@ import puzzle
 import json
 
 
-def onetwo(INPUT):
+def onetwo(INPUT, two=False):
   def sum_num(inval):
     val = 0
-    print(type(inval))
     if type(inval) == type(0):
       return inval
     if type(inval) == type(""):
       return 0
-    # Comment this out for part one
-    if type(inval) == type({}) and 'red' in inval.values():
+    if two and type(inval) == type({}) and 'red' in inval.values():
       return 0
     keys = range(len(inval)) if type(inval) == type([]) else inval.keys()
     for k in keys: val += sum_num(inval[k])
     return val
 
-  return sum_num(json.loads(INPUT))
+  return sum_num(json.loads(INPUT[0]))
+
+def one(INPUT):
+  return onetwo(INPUT, two=False)
+
+def two(INPUT):
+  return onetwo(INPUT, two=True)
 
 p = puzzle.Puzzle("2015", "12")
-p.run(onetwo, 0)
+p.run(one, 0)
+p.run(two, 0)
