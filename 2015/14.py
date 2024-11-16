4091926f439bb1c785839f34e01cd6010915b67c
Harvey Jones <harveyj@gmail.com>
2024-11-16 12:38:33 -0500

    modernize 14

--- 2015/14.py
+++ 2015/14.py
@@ -6,25 +6,25 @@ import collections
 
 def parse(INPUT):
   pat = re.compile('(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.')
-  for l in INPUT.split('\n'):
+  for l in INPUT:
     yield re.match(pat, l).groups()
 
 
 def one(INPUT):
   total_t = 2503
   reindeer = parse(INPUT)
+  score_max = 0
   for r in reindeer:
     dist, fly_t, rest_t = int(r[1]), int(r[2]), int(r[3])
-    # print(fly_t, rest_t)
     cycle_t = fly_t + rest_t
     cycles = total_t // cycle_t
     rem = total_t % cycle_t
     rem = min(fly_t, rem)
-    # print(cycles, cycle_t, rem, fly_t)
-    print(r[0], cycles*fly_t*dist + rem*dist)
-  return 0
+    score = cycles*fly_t*dist + rem*dist
+    if score > score_max:
+      score_max = score
+  return score_max
 
-# there was an off by one somewhere in there...
 def two(INPUT):
   total_t = 2503
   reindeer = list(parse(INPUT))
@@ -42,13 +42,11 @@ def two(INPUT):
   for t in range(1, total_t):
     dists = {r : r_dists[r][t] for r in r_dists.keys()}
     max_score = max(dists.values())
-    # print(t, reindeer)
     for r in reindeer:
-      print(r)
       if r_dists[r[0]][t] == max_score: scores[r] += 1
-  print(scores)
-  return 0
+  # print(scores)
+  return max(scores.values())
 
 p = puzzle.Puzzle("2015", "14")
-# p.run(one, 0)
+p.run(one, 0)
 p.run(two, 0)
