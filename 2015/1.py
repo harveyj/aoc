298d1cfe64f61d3a541a1c0de0b3db4cb4735b49
Harvey Jones <harveyj@gmail.com>
2024-11-16 11:13:57 -0500

    modernize 2015-1

--- 2015/1.py
+++ 2015/1.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 import puzzle
-import re
-import networkx as nx
-
-def parse(INPUT):
-  return 0
 
 def one(INPUT):
-  return INPUT.count('(') - INPUT.count(')')
+  return INPUT[0].count('(') - INPUT[0].count(')')
 
 def two(INPUT):
   floor = 0
-  for i, c in enumerate(INPUT):
+  for i, c in enumerate(INPUT[0]):
     if c == '(': floor += 1
     if c == ')': floor -= 1
     if floor < 0:
-      return i
-p = puzzle.Puzzle("1")
+      return i+1
+
+p = puzzle.Puzzle("2015", "1")
+
 p.run(one, 0)
 p.run(two, 0)
---
+++ 2015/puzzle-old.py
@@ -0,0 +1,14 @@
+import copy
+
+class Puzzle(object):
+  def __init__(self, id):
+    self.id = id
+    self.inputs = open('inputs/' + id + '.txt').read().split('\n\n\n')
+
+  def run(self, fn, input_id, user_input=None, **kwarg):
+    if user_input:
+      answer =  fn(self.inputs[input_id], copy.copy(user_input), **kwarg)
+    else: 
+      answer = fn(self.inputs[input_id], **kwarg)
+    print("ANSWER", answer)
+    return answer
