e00dc8c06c383d5bb104d9fb05f2cce3f8764686
Harvey Jones <harveyj@gmail.com>
2024-11-17 10:50:09 -0500

    oh wow 2018 redo

---
+++ 2018/1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import puzzle, re
+
+def parse_input(INPUT):
+  return INPUT
+
+def one(INPUT):
+  out = 0
+  for chunk in INPUT:
+    out += eval(chunk)
+  return out
+
+def two(INPUT):
+  out = 0
+  seen = set()
+  while True:
+    for chunk in INPUT:
+      out += eval(chunk)
+      if out in seen:
+        return out
+      seen.add(out)
+
+p = puzzle.Puzzle("2018", "1")
+p.run(one, 0)
+p.run(two, 0)
