54bc353ac9db25a5d486fb966c8505fa374a8fb5
Harvey Jones <harveyj@gmail.com>
2024-12-06 07:17:05 -0500

    2024-5 and readme updates

---
+++ 2024/5.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import puzzle, re
+import networkx as nx
+
+def one(INPUT):
+  raw = '\n'.join(INPUT)
+  rules, packets = raw.split('\n\n')
+  rules = rules.split('\n')
+  packets = packets.split('\n')
+
+  good = []
+  illegal_packets = []
+  for packet in packets:
+    vals = packet.split(',')
+    forbidden = set()
+    for l in rules:
+      a, b = l.split('|')
+      if a in vals:
+        forbidden.add((b, a))
+    illegal_vals = [(v, v2) for v, v2 in zip(vals, vals[1:]) if (v, v2) in forbidden]
+
+    if len(illegal_vals) == 0: 
+      good.append(int(vals[len(vals)//2]))
+    else: 
+      illegal_packets.append(packet)
+  return sum(good), illegal_packets
+
+def two(INPUT):
+  raw = '\n'.join(INPUT)
+  rules, _ = raw.split('\n\n')
+  rules = rules.split('\n')
+  rules = [r.split('|') for r in rules]
+  _, illegal_packets = one(INPUT)
+  # print(illegal_packets)
+  out = []
+  for packet in illegal_packets:
+    vals = packet.split(',')
+    G = nx.DiGraph()
+    filtered_rules = [r for r in rules if r[0] in vals and r[1] in vals]
+    print(filtered_rules)
+    for r in filtered_rules:
+      G.add_edge(*r)
+    sorted_packet = list(map(int, nx.topological_sort(G)))
+    out.append(int(sorted_packet[len(sorted_packet)//2]))
+  return sum(out)
+
+p = puzzle.Puzzle("2024", "5")
+p.run(one, 0)
+p.run(two, 0)
--- 2024/README.md
+++ 2024/README.md
@@ -5,3 +5,26 @@ Another pure Python year. Capen year 2!
 Started 12/2/2024, finished ??.
 
 ## Day 1
+Pure Warmup. Done from Stamford Hospital with Thomas <3
+
+Difficulty: 1/10
+
+# Day 2
+Done from Stamford Hospital. Kinda fiddly for day 2!
+
+Difficulty: 3/10
+
+# Day 3
+Regex + state machine. Pretty fun! Done from Stamford Hospital.
+
+Difficulty: 3.5/10
+
+## Day 4
+Word search. Done at home while Katie and Helen watched Thomas <3
+
+Difficulty: 4/10
+
+## Day 5
+Create ruleset. Topological sort. Done from Thomas's nursery.
+
+Difficulty: 5/10
