0ae11338c44f875cf8a292fd45fb4994c133026d
Harvey Jones <harveyj@gmail.com>
2024-12-09 17:27:06 -0500

    add timing to the automated runner

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import importlib.util
 import argparse
 import csv
 import puzzle
+import time
 
 parser = argparse.ArgumentParser(description="Run advent of code days in an automated fashion.")
 
@@ -17,10 +18,12 @@ if args.year: years = [args.year]
 if args.days: days = args.days.split(',')
 
 
+
 for year in years:
   print(f'Year {year}')
   answers = list(csv.reader(open(f'{year}/inputs/answers.txt')))
   answers = {a[0]: [eval(a[1]), eval(a[2])] for a in answers}
+  timings = dict()
   for i in days:
     print(f'Day {i}')
     file_path = f'{year}/{i}.py'
@@ -29,8 +32,14 @@ for year in years:
     day = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(day)
     p = puzzle.Puzzle(f'{year}', f"{i}")
+    start_time = time.time()
     one = p.run(day.one, 0) 
+    end_time = time.time()
+    timings[f'{year}-{i}-1'] = end_time - start_time
+    start_time = time.time()
     two = p.run(day.two, 0) 
+    end_time = time.time()
+    timings[f'{year}-{i}-2'] = end_time - start_time
     if answers[str(i)][0] != str(one):
       print(f'INCORRECT: output - {one} vs correct - {answers[str(i)][0]}')
       continue
@@ -38,3 +47,6 @@ for year in years:
       print(f'INCORRECT: output - {two} vs correct - {answers[str(i)][1]}')
       continue
     print('CORRECT')
+  for t in timings:
+    print(f'{t} {round(timings[t]* 1000000) / 1000} ms')
+    
