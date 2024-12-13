6908133d8a9b20789125bfec436ba2076e8aceed
Harvey Jones <harveyj@gmail.com>
2024-12-13 11:54:14 -0500

    2024-13-1 and 2

--- 2024/13.py
+++ 2024/13.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
-import puzzle, re, library
-from collections import defaultdict
-
+import puzzle, library
+from sympy import symbols, Eq, solve
 
 def parse_input(INPUT):
-  return INPUT
+  chunks = '\n'.join(INPUT).split('\n\n')
+  for chunk in chunks:
+    a, b, prize = chunk.split('\n')
+    yield library.ints(a), library.ints(b), library.ints(prize)
 
-def one(INPUT):
-  invals = parse_input(INPUT)
-  out = 0
-  return out
+def one(INPUT, two=False):
+  all = list(parse_input(INPUT))
+  tot = 0
+  for a, b, prize in all:
+    ax, ay = a; bx, by = b; px, py = prize
+    if two: 
+      px += 10000000000000; py += 10000000000000
+    a_press, b_press = symbols('a, b')
+    eq1 = Eq(ax*a_press + bx*b_press, px)
+    eq2 = Eq(ay*a_press + by*b_press, py)
 
-def two(INPUT):
-  invals = parse_input(INPUT)
-  out = 0
-  return out
+    solution = solve([eq1, eq2], (a_press, b_press))
+    if int(solution[a_press]) == solution[a_press] and int(solution[b_press]) == solution[b_press]:
+      tot += 3 * int(solution[a_press]) + 1 * int(solution[b_press])
+  return tot
 
-
-  # G = library.Grid(raw='\n'.join(INPUT))
-  # for x in range(G.max_x()):
-  #   for y in range(G.max_y()):
-  #     c = G.get((x, y))
+two = lambda a: one(a, two=True)
 
 if __name__ == '__main__':
-  p = puzzle.Puzzle("2024", "")
+  p = puzzle.Puzzle("2024", "13")
   print(f'ANSWER: {p.run(one, 0)}')
   print(f'ANSWER: {p.run(two, 0)}')
