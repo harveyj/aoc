a33913f2fd172cb5da49f0a4cb3a495104699673
Harvey Jones <harveyj@gmail.com>
2024-12-17 09:49:58 -0500

    very barebones scaffolding

---
+++ 2020/test.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin/node
+const fs = require('node:fs');
+
+
+fs.readFile('./inputs/answers.txt', 'utf8', (err, data) => {
+  check(data);
+});
+
+function check(answers) {
+  console.log(answers)
+for (let i = 1; i < 26; i++) {
+  const day = require(`./day${i}.js`);
+  fs.readFile(`./inputs/${i}.txt`, 'utf8', (err, data) => {
+    let input = day._input(data);
+    let answer1 = day._ANSWER_1(input);
+    let answer2 = day._ANSWER_2(input);
+  }); 
+};
+}
