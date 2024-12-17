d9bad9e7ebe1e6bbc4f9f7129c9a98a297b17383
Harvey Jones <harveyj@gmail.com>
2024-12-17 10:10:20 -0500

    indent in test rig

--- 2020/test.js
+++ 2020/test.js
@@ -8,12 +8,12 @@ fs.readFile('./inputs/answers.txt', 'utf8', (err, data) => {
 
 function check(answers) {
   console.log(answers)
-for (let i = 1; i < 26; i++) {
-  const day = require(`./day${i}.js`);
-  fs.readFile(`./inputs/${i}.txt`, 'utf8', (err, data) => {
-    let input = day._input(data);
-    let answer1 = day._ANSWER_1(input);
-    let answer2 = day._ANSWER_2(input);
-  }); 
-};
+  for (let i = 1; i < 26; i++) {
+    const day = require(`./day${i}.js`);
+    fs.readFile(`./inputs/${i}.txt`, 'utf8', (err, data) => {
+      let input = day._input(data);
+      let answer1 = day._ANSWER_1(input);
+      let answer2 = day._ANSWER_2(input);
+    }); 
+  };
 }
