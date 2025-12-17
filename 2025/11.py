2439cb9999e065db1f2f56a54d51cd444c81683f
Harvey Jones <harveyj@gmail.com>
2025-12-17 00:07:44 -0500

    trimming

--- 2025/11.py
+++ 2025/11.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import puzzle
 import networkx as nx
-import matplotlib.pyplot as plt
-from networkx.drawing.nx_agraph import graphviz_layout
 from functools import lru_cache
 
 @lru_cache()
@@ -11,10 +9,8 @@ def count_paths(DG, src, dst):
     return 1
   return sum([count_paths(DG, node, dst) for node in DG.successors(src)])
 
-
 def one(INPUT, two=False):
   DG = nx.DiGraph()
-  RDG = nx.DiGraph()
   nodes = []
   for l in INPUT:
     fields = l.split()
@@ -23,18 +19,14 @@ def one(INPUT, two=False):
   for node in nodes:
     for dst in node[1:]:
       DG.add_edge(node[0], dst)
-      RDG.add_edge(dst, node[0])
 
   if two:
     # no paths fft-dac in my input. route must be svr-dac-fft-out
-    print(count_paths(DG, "svr", "fft"), count_paths(DG, "fft", "dac"), count_paths(DG, "dac", "out") 
-)
     return count_paths(DG, "svr", "fft") * count_paths(DG, "fft", "dac") * count_paths(DG, "dac", "out") 
   return len(list(nx.all_simple_paths(DG, 'you', 'out')))
 
 def two(INPUT):
   return one(INPUT, two=True)
-
 
 if __name__ == '__main__':
   p = puzzle.Puzzle("2025", "11")
